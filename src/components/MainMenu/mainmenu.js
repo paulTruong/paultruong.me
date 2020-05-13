fb7a2e13ba566235dcc88108a3be74418b69c6ba
Paul Truong <mrpaultruong@gmail.com>
2020-05-13 19:44:23 +1000

    Update mainmenu.js

--- src/components/MainMenu/mainmenu.js
+++ src/components/MainMenu/mainmenu.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import MainMenuStyles from "./MainMenu.module.css"
+import MainMenuStyles from "./mainmenu.module.css"
 
 const MainMenu = () => (
   <nav>
